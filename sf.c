ccab9c9213e3abde74df91a5ced1532a16bbba4f
ysu <ysu@FBSD.hsd1.co.comcast.net>
2021-05-07 12:45:11 -0600

    added uptime file with support for linux and freebsd and updated makefile

--- sf.c
+++ sf.c
@@ -2,20 +2,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/utsname.h>
-#ifdef __linux__
-	#include <sys/sysinfo.h>
-	struct sysinfo system;
-	sysinfo(&system);
-#endif
 #include <string.h>
+#include <inttypes.h>
 #include <pwd.h>
+
+#include "uptime.h"
 
 /* help links:
  * https://www.tutorialspoint.com/unix_system_calls/sysinfo.htm
  */
 
 char *getOS(){
-	int i = 0, pos = 7;
+	int i = 0, pos = 6;
 	char buff[70], *res = malloc (sizeof (char) * 255);
 	FILE *osr;
 	if(!(osr = fopen("/etc/os-release", "r"))) { return "Unable to find OS"; }
@@ -24,7 +22,7 @@ char *getOS(){
 	fclose(osr);
 
 	while (i < 63 && buff[pos + i - 1] != '\"') {
-		res[i] = buff[pos + i - 1];
+		if(buff[pos + i - 1] != '\"' && buff[pos + i - 1] != '\n') { res[i] = buff[pos + i - 1]; }
 		i++;
 	} res[i] = '\0';
 	
@@ -45,7 +43,7 @@ char *getSecondToken(char str[], const char delim[]) {
 }
 
 char *getRam(){
-  FILE *meminfo;
+  	FILE *meminfo;
 	if(!(meminfo = fopen("/proc/meminfo", "r"))) { return "Unable to get memory stats"; }
 	char buff[255], buff2[255], *res = malloc (sizeof (char) * 255);
 
@@ -62,12 +60,12 @@ char *getRam(){
 int main(){
 	struct utsname name;
 	uname(&name);
-
+	
 	printf("\033[1m\033[36m%s@\033[1m\033[37m%s\n", getUserName(),name.nodename);
 	printf("\033[1m\033[36mos\t\033[0;37m%s\n", getOS());
 	printf("\033[1m\033[36mkernel\t\033[0;37m%s\n", name.release);
 	printf("\033[1m\033[36mshell\t\033[0;37m%s\n", getenv("SHELL"));
-	printf("\033[1m\033[36muptime\t\033[0;37m%dh %dm\n", system.uptime/60/60, system.uptime/60 % 60);
+	printf("\033[1m\033[36muptime\t\033[0;37m%"PRIu64"h %"PRIu64"m\n", getUptime()/60/60, getUptime()/60 % 60);
 	printf("\033[1m\033[36mmemory\t\033[0;37m%s\033[0m\n", getRam());
 
 	return 0;
